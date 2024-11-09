84fca34d2ff3e6bf4e72a963e1da9b1ed277acc6
Alain <zakab.66gmail.com>
2024-11-09 15:39:15 +0100

    fix leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,20 @@
 /*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:24:40 by asene             #+#    #+#             */
-/*   Updated: 2024/11/09 14:44:27 by asene            ###   ########.fr       */
+/*   Updated: 2024/11/09 15:39:08 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static void	*replace_pointer(void **dest, void *new)
+{
+	void *temp;
+
+	temp = *dest;
+	*dest = new;
+	free(temp);
+}
 
 static char *join_and_free(char **buffer, char *new_content)
 {
@@ -49,18 +58,25 @@ static void	read_file_to_next_line(int fd, char **buffer, char **next_eol)
 char	*get_next_line(int	fd)
 {
 	static char	*buffer = NULL;
+	static int	file_end = 0;
 	char		*line;
 	char		*eol;
 	char		*temp;
 
+	if (file_end)
+		return NULL;
 	if (buffer == NULL)
-		buffer = malloc(BUFFER_SIZE);
+		buffer = malloc(BUFFER_SIZE + 1);
 	eol = ft_strchr(buffer, '\n');
 	if (eol == NULL)
 		read_file_to_next_line(fd, &buffer, &eol);
 	line = ft_substr(buffer, 0, eol - buffer + 1);
-	temp = ft_strdup(eol + 1);
-	free(buffer);
-	buffer = temp;
+	if (eol && *(eol + 1))
+		replace_pointer((void **)&buffer, ft_strdup(eol + 1));
+	else
+	{
+		file_end = 1;
+		free(buffer);
+	}
 	return (line);
 }
